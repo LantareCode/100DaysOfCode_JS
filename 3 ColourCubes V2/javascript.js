0a40c2460300f16a4ea7b3c8ab92d223b66421b4
Valeske <lantare.code@gmail.com>
2016-06-24 16:49:37 +0200

    Day3-5 ColourCubes V2 (Not Complete yet)
    Started adding javascript, #start and #stop have some functionality.

--- 3 ColourCubes V2/javascript.js
+++ 3 ColourCubes V2/javascript.js
@@ -1,31 +1,81 @@
-//document.getElementById("cube1").style.display = "none";
-
-
-//document.getElementById("cube1").style.visibility = "visible";
-//document.getElementById("cube1").style.visibility = "hidden";
-
+//#start Clicked
+//  calls hideAndReset()
+//  calls cubeAppear() which starts the game.
+var startTimer;
 document.getElementById("start").onclick = function(){
     hideAndReset();
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    startTimer = setTimeout(cubeAppear, Math.random()*2000);
+}
+
+//cubeAppear function:
+//  Makes cubes appear at random times
+//  Calls disappearTimer function at a random time.
+var appearTimer;
+function cubeAppear(){
+    document.getElementById("cube1").style.visibility = "visible";
+    appearTimer = setTimeout(cubeDisappear, Math.random()*2000);
+
+}
+
+//cubeDisappear function:
+//  Makes cubes disappear at random times
+//  Calls cubeAppear function at a random time.
+var disappearTimer;
+function cubeDisappear(){
+    document.getElementById("cube1").style.visibility = "hidden";
+    disappearTimer = setTimeout(cubeAppear, ((Math.random()*5000) + 2000));
 }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//First function called on #start is clicked on:
+//  Hides all cubes
+//  Clear score
+//  Displays #stop div.
 function hideAndReset(){
     document.getElementById("cube1").style.visibility = "hidden";document.getElementById("cube2").style.visibility = "hidden";document.getElementById("cube3").style.visibility = "hidden";
     document.getElementById("cube4").style.visibility = "hidden";document.getElementById("cube5").style.visibility = "hidden";document.getElementById("cube6").style.visibility = "hidden";    
-    document.getElementById("cube7").style.visibility = "hidden"; 
+    document.getElementById("cube7").style.visibility = "hidden";     
+
+    document.getElementById("score_points").innerHTML = "0000000";
+
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("stop").style.visibility = "visible";    
+}
+//#stop Clicked
+//  Clears Timeouts
+//  Display all cubes
+//  Dispay #start and hide #stop
+document.getElementById("stop").onclick = function(){
+    clearTimeout(startTimer);
+    clearTimeout(appearTimer);
+    clearTimeout(disappearTimer);    
     
-    //TO DO - Change start to stop.
-    document.getElementById("start").innerHTML = "S T O P";
-    //TO DO - Reset score to 0000000
+    document.getElementById("cube1").style.visibility = "visible";document.getElementById("cube2").style.visibility = "visible";document.getElementById("cube3").style.visibility = "visible";
+    document.getElementById("cube4").style.visibility = "visible";document.getElementById("cube5").style.visibility = "visible";document.getElementById("cube6").style.visibility = "visible";    
+    document.getElementById("cube7").style.visibility = "visible";
+    
+    document.getElementById("start").style.visibility = "visible";
+    document.getElementById("stop").style.visibility = "hidden";
 }
