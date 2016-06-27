6f99f86cac5db278ed3a397c9dd01fadbeaedfef
Valeske <lantare.code@gmail.com>
2016-06-27 20:12:09 +0200

    Day3-8 ColourCubes V2 (Not Complete yet)
    Added a temp workaround, but things to fix:
    - Cubes can be clicked before game is started(temp workaround fixed
    this)
    - Cubes don't stop flashing after they have been clicked.

--- 3 ColourCubes V2/javascript.js
+++ 3 ColourCubes V2/javascript.js
@@ -1,69 +1,3 @@
-//#start Clicked
-//  calls hideAndReset()
-//  calls cubeAppear() which starts the game.
-var startTimer;
-document.getElementById("start").onclick = function(){
-    hideAndReset();
-    var startTimeout = Math.random()*2000;
-    startTimer = setTimeout(cubeAppear_1, startTimeout);
-    startTimer = setTimeout(cubeAppear_2, startTimeout);
-    startTimer = setTimeout(cubeAppear_3, startTimeout);
-    startTimer = setTimeout(cubeAppear_4, startTimeout);
-    startTimer = setTimeout(cubeAppear_5, startTimeout);
-    startTimer = setTimeout(cubeAppear_6, startTimeout);
-    startTimer = setTimeout(cubeAppear_7, startTimeout);        
-}
-//First function called on #start is clicked on:
-//  Hides all cubes
-//  Clear score
-//  Displays #stop div.
-function hideAndReset(){
-    document.getElementById("cube1").style.visibility = "hidden";document.getElementById("cube2").style.visibility = "hidden";document.getElementById("cube3").style.visibility = "hidden";
-    document.getElementById("cube4").style.visibility = "hidden";document.getElementById("cube5").style.visibility = "hidden";document.getElementById("cube6").style.visibility = "hidden";    
-    document.getElementById("cube7").style.visibility = "hidden";     
-
-    document.getElementById("score_points").innerHTML = "0";
-
-    document.getElementById("start").style.visibility = "hidden";
-    document.getElementById("stop").style.visibility = "visible";    
-}
-
-
-//cubeAppear function:
-//  Makes cubes appear at random times
-//  Calls disappearTimer function at a random time.
-function callDisappear(funcName){   
-    return (setTimeout(funcName, 5000));
-}
-var appearTimer1;   function cubeAppear_1(){
-    document.getElementById("cube1").style.visibility = "visible";
-    appearTimer1 = callDisappear(cubeDisappear_1);    
-}
-var appearTimer2;   function cubeAppear_2(){
-    document.getElementById("cube2").style.visibility = "visible";
-    appearTimer2 = callDisappear(cubeDisappear_2);    
-}
-var appearTimer3;   function cubeAppear_3(){
-    document.getElementById("cube3").style.visibility = "visible";
-    appearTimer3 = callDisappear(cubeDisappear_3); 
-}
-var appearTimer4;   function cubeAppear_4(){
-    document.getElementById("cube4").style.visibility = "visible";
-    appearTimer4 = callDisappear(cubeDisappear_4); 
-}
-var appearTimer5;   function cubeAppear_5(){
-    document.getElementById("cube5").style.visibility = "visible";
-    appearTimer5 = callDisappear(cubeDisappear_5); 
-}
-var appearTimer6;   function cubeAppear_6(){
-    document.getElementById("cube6").style.visibility = "visible";
-    appearTimer6 = callDisappear(cubeDisappear_6); 
-}
-var appearTimer7;   function cubeAppear_7(){
-    document.getElementById("cube7").style.visibility = "visible";
-    appearTimer7 = callDisappear(cubeDisappear_7); 
-}
-
 //cubeDisappear function:
 //  Makes cubes disappear at random times
 //  Calls cubeAppear function at a random time.
@@ -100,11 +34,43 @@ var disappearTimer7;    function cubeDisappear_7(){
     disappearTimer7 = callAppear(cubeAppear_7);
 }
 
+//cubeAppear function:
+//  Makes cubes appear at random times
+//  Calls disappearTimer function at a random time.
+function callDisappear(funcName){   
+    return (setTimeout(funcName, 5000));
+}
+var appearTimer1;   function cubeAppear_1(){
+    document.getElementById("cube1").style.visibility = "visible";
+    appearTimer1 = callDisappear(cubeDisappear_1);    
+}
+var appearTimer2;   function cubeAppear_2(){
+    document.getElementById("cube2").style.visibility = "visible";
+    appearTimer2 = callDisappear(cubeDisappear_2);    
+}
+var appearTimer3;   function cubeAppear_3(){
+    document.getElementById("cube3").style.visibility = "visible";
+    appearTimer3 = callDisappear(cubeDisappear_3); 
+}
+var appearTimer4;   function cubeAppear_4(){
+    document.getElementById("cube4").style.visibility = "visible";
+    appearTimer4 = callDisappear(cubeDisappear_4); 
+}
+var appearTimer5;   function cubeAppear_5(){
+    document.getElementById("cube5").style.visibility = "visible";
+    appearTimer5 = callDisappear(cubeDisappear_5); 
+}
+var appearTimer6;   function cubeAppear_6(){
+    document.getElementById("cube6").style.visibility = "visible";
+    appearTimer6 = callDisappear(cubeDisappear_6); 
+}
+var appearTimer7;   function cubeAppear_7(){
+    document.getElementById("cube7").style.visibility = "visible";
+    appearTimer7 = callDisappear(cubeDisappear_7); 
+}
 
 
 
-
-/********************/
 //Click on a cube:
 //  hide the cube
 //  add point to #score_points
@@ -143,14 +109,57 @@ document.getElementById("cube7").onclick = function (){
     cubeDisappear_7();  
     addPoints();    
 }
+
+
+
+
 /********************/
+//#start Clicked
+//  calls hideAndReset()
+//  calls cubeAppear() which starts the game.
+var startTimer;
+document.getElementById("start").onclick = function(){
+    hideAndReset();    
+    
+    var startTimeout = Math.random()*2000;        
+    
+    startTimer = setTimeout(cubeAppear_1, startTimeout);
+    startTimer = setTimeout(cubeAppear_2, startTimeout);
+    startTimer = setTimeout(cubeAppear_3, startTimeout);
+    startTimer = setTimeout(cubeAppear_4, startTimeout);
+    startTimer = setTimeout(cubeAppear_5, startTimeout);
+    startTimer = setTimeout(cubeAppear_6, startTimeout);
+    startTimer = setTimeout(cubeAppear_7, startTimeout);      
+}
+//First function called on #start is clicked on:
+//  Hides all cubes
+//  Clear score
+//  Displays #stop div.
+function hideAndReset(){  
+    document.getElementById("cube1").style.visibility = "hidden";document.getElementById("cube2").style.visibility = "hidden";document.getElementById("cube3").style.visibility = "hidden";
+    document.getElementById("cube4").style.visibility = "hidden";document.getElementById("cube5").style.visibility = "hidden";document.getElementById("cube6").style.visibility = "hidden";    
+    document.getElementById("cube7").style.visibility = "hidden";     
 
+    document.getElementById("score_points").innerHTML = "0";
+    
+    document.getElementById("game").style.display = "block";
+    document.getElementById("startScreen").style.display = "none";
+    
 
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("stop").style.visibility = "visible";    
+}
 //#stop Clicked
 //  Clears Timeouts
 //  Display all cubes
 //  Dispay #start and hide #stop
 document.getElementById("stop").onclick = function(){
+    document.getElementById("game").style.display = "none";
+    document.getElementById("startScreen").style.display = "block";
+    
+    points = 0;
+    startTimeout = 2000;
+    
     clearTimeout(startTimer);   
     
     clearTimeout(appearTimer1); clearTimeout(disappearTimer1);
