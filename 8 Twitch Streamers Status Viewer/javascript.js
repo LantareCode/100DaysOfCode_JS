ff7ce2e6a9028f0608dd19d965194dfeb43c8141
Valeske <lantare.code@gmail.com>
2016-07-22 16:02:26 +0200

    Day33 Worked on the TwitchStream Project

--- 8 Twitch Streamers Status Viewer/javascript.js
+++ 8 Twitch Streamers Status Viewer/javascript.js
@@ -1,13 +1,28 @@
 $(document).ready(function (){        
     var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    
+    var channel = 'sheevergaming';
 
-    const apiUrl = '';   
+    const apiUrl = 'https://api.twitch.tv/kraken/streams/'+channel+'?callback=?';   
 
     $.ajax({
         url : apiUrl,
         method : 'GET',
         dataType: "jsonp",
         success : function (channel_info) { 
+            
+            
+            $('#name').html(channel);
+            
+            if(channel_info.stream === null)
+                $('#status').html('offline');           
+            else if(channel_info.stream === undefined)                
+                $('#status').html('account closed');           
+            else
+                $('#status').html(channel_info.stream.game);  
+            
+            
+            console.log(channel_info);
             //$('#quote').html(channel_info.quote),
             
         }   
